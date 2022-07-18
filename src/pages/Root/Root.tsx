9017a6b267a54f86989210e9d0385b5a0807485f
Maksim <krovorgen@gmail.com>
2022-07-18 22:49:05 +0300

    roshan

--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Input } from '@alfalab/core-components/input';
+import { Button } from '@alfalab/core-components/button';
+
+import styles from './Root.module.scss';
 
 export const Root = () => {
-  return <h1>Hello world!</h1>;
+  const [value, setValue] = useState('');
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setValue('');
+    const [firstTime, secondTime] = value.split(' ');
+    await navigator.clipboard.writeText(
+      `Kill roshan in ${value}: respawn from ${+firstTime + 8}:${secondTime} to ${
+        +firstTime + 11
+      }:${secondTime}`,
+    );
+  };
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+  return (
+    <form className={styles.form} onSubmit={onSubmit}>
+      <Input required block onChange={onChange} value={value} />
+      <Button type="submit" block>
+        Расчитать
+      </Button>
+    </form>
+  );
 };
